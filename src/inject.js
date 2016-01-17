8217850cc8251c424b93607255babc6ea477ef40
Martin Lissmyr <martin@Martins-iMac.local>
2016-01-17 16:18:55 +0100

    Prevents unwanted file drop events on Inbox

--- src/inject.js
+++ src/inject.js
@@ -1,4 +1,5 @@
 var ipc = require("electron").ipcRenderer;
+var isDragEvent = false;
 
 // Pass all clicked external links to host
 document.addEventListener("click", function(event) {
@@ -8,6 +9,25 @@ document.addEventListener("click", function(event) {
    }
    ipc.sendToHost("external-link-clicked", clickedEl.href);
 });
+
+window.addEventListener("dragover", function(event) {
+  // Register that a drag event is under way
+  isDragEvent = true;
+});
+
+window.addEventListener("dragleave", function(event) {
+  // Unregister drag event
+  isDragEvent = false;
+});
+
+window.addEventListener("beforeunload", function(event) {
+  // If the current window is about to unload, cancel it...
+  // But only cancel it if it's the result of a drag-n-drop event
+  // otherwise we make it impossible to log in
+  if (isDragEvent) {
+    event.returnValue = "false";
+  }
+}, false);
 
 // Listen for requests to update unread messages count
 ipc.on("do-unread-count", function() {
